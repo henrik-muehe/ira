ac63a62805551f063c237ba1ebb594173945c362
Henrik Mühe <muehe@in.tum.de>
2012-11-23 09:48:23 +0100

    added license and readme

--- js/expression_outer_join.js
+++ js/expression_outer_join.js
@@ -1,3 +1,20 @@
+/* 
+IRA - Interactive Relational Algebra Tool
+Copyright (C) 2010-2012 Henrik Mühe
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 function OuterJoin(input1, input2) {
     var symbol = "\\hspace{1ex}{\\mathrel{\\stackrel{\\rule[0.52ex]{0.50ex}{0.10ex}}{\\rule[0ex]{0.50ex}{0.10ex}}\\hspace{-0.75ex}\\mathord{\\bowtie}\\hspace{-0.1ex}{\\mathrel{\\stackrel{\\rule[0.52ex]{0.50ex}{0.10ex}}{\\rule[0ex]{0.50ex}{0.10ex}}\\hspace{-0.7ex}}}}}\\hspace{1ex} ";
     this.setChildren([input1, input2]);
